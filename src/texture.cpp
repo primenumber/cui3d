988a2e1e14cec0f5bd3de99af1c259c9325033d1
prime <prime@kmc.gr.jp>
2015-09-22 09:32:00 +0900

    fix texture

--- src/texture.cpp
+++ src/texture.cpp
@@ -4,7 +4,7 @@ namespace cui3d {
 
 Pixel PlaneMappingTexture::operator()(const Vec3D &vec) {
   Vec3D origined_vec = vec - origin;
-  return Pixel('@', Color::RED, Color::BLACK);
+  return Pixel('@', Color::RED, Color::WHITE);
 }
 
 } // namespace cui3d
