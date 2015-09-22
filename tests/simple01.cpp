2d78759ef849f532f96659ff609021d9b9224472
prime <prime@kmc.gr.jp>
2015-09-22 10:32:00 +0900

    fix timer

--- tests/simple01.cpp
+++ tests/simple01.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <tuple>
 #include <sys/time.h>
-#include <boost/timer.hpp>
+#include <boost/timer/timer.hpp>
 
 cui3d::CuiImage draw(int h, int w, double t) {
   using namespace cui3d;
@@ -17,6 +17,8 @@ cui3d::CuiImage draw(int h, int w, double t) {
       Vec3D(-t*1.6+0.5,0.1, 0.3)));
   p.emplace_back(applyTransform(cui3d::make_cuboid(Vec3D(-0.15, -0.15, 0.0),
       Vec3D(0.15, 0.15, 0.3)), rotateZ(t)));
+  p.emplace_back(applyTransform(cui3d::make_cuboid(Vec3D(-0.45, -0.45, 0.1),
+      Vec3D(-0.15, -0.15, 0.4)), rotateX(-t)));
   p[0].texture = PlaneMappingTexture();
   p[1].texture = PlaneMappingTexture();
   p[2].texture = PlaneMappingTexture();
@@ -32,19 +34,17 @@ std::tuple<int, int> fix_size(int h, int w, double ratio = 2.0) {
 
 int main() {
   cui3d::Screen scr;
-  for (int i = 0; i < 200; ++i) {
+  for (int i = 0; i < 1200; ++i) {
     double t = i/200.0;
-    double process_time = 0;
-    boost::timer tm;
+    boost::timer::cpu_timer tm;
     int h, w;
     std::tie(h, w) = fix_size(scr.get_height(), scr.get_width(), 2.5);
     cui3d::CuiImage img = draw(h, w, t);
     scr.draw(img);
     scr.render();
-    process_time += tm.elapsed();
-    double rem = 1.0/60 - process_time;
+    double rem = 1e+9/60.0 - tm.elapsed().wall;
     if (rem > 0) {
-      timespec ts = {0, (long)(rem * 1000000000)};
+      timespec ts = {0, (long)rem};
       nanosleep(&ts, nullptr);
     }
   }
